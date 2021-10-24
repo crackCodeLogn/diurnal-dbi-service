cd9389b420569c31b8e1e01c25ba73b974206696
Vivek <vivektheintel@gmail.com>
2021-10-24 14:23:06 +0530

    Updating all tests to junit5 and assertj

--- src/test/java/com/vv/personal/diurnal/dbi/controller/DataControllerTest.java
+++ src/test/java/com/vv/personal/diurnal/dbi/controller/DataControllerTest.java
@@ -3,17 +3,19 @@ package com.vv.personal.diurnal.dbi.controller;
 import com.vv.personal.diurnal.artifactory.generated.EntryDayProto;
 import com.vv.personal.diurnal.artifactory.generated.ResponsePrimitiveProto;
 import com.vv.personal.diurnal.artifactory.generated.UserMappingProto;
+import com.vv.personal.diurnal.dbi.config.DbiConfig;
 import com.vv.personal.diurnal.dbi.config.GenericConfig;
 import com.vv.personal.diurnal.dbi.interactor.diurnal.dbi.tables.DiurnalTableEntryDay;
 import com.vv.personal.diurnal.dbi.util.DiurnalUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,8 +27,7 @@ import java.util.List;
 
 import static com.vv.personal.diurnal.dbi.util.DiurnalUtil.generateHash;
 import static com.vv.personal.diurnal.dbi.util.DiurnalUtil.procureStopWatch;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -34,7 +35,7 @@ import static org.mockito.Mockito.when;
  * @author Vivek
  * @since 03/03/21
  */
-@RunWith(MockitoJUnitRunner.class)
+@ExtendWith(MockitoExtension.class)
 public class DataControllerTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataControllerTest.class);
 
@@ -63,13 +64,13 @@ public class DataControllerTest {
         return data;
     }
 
-    @Before
+    @BeforeEach
     public void preHaste() {
         dataController.setEntryDayController(entryDayController);
     }
 
     @Test
-    public void testPushWholeBackup() {
+    void testPushWholeBackup() {
         List<String> testData = readFileFromLocation("src/test/resources/sample.backup.txt");
         System.out.println(testData);
         long mobile = 1234567890L;
@@ -79,7 +80,6 @@ public class DataControllerTest {
         when(userMappingController.retrieveHashEmail(email)).thenReturn(emailHash);
         when(userMappingController.retrievePremiumUserStatus(emailHash)).thenReturn(true);
         when(userMappingController.updateUserMappingLastCloudSaveTimestamp(any(UserMappingProto.UserMapping.class))).thenReturn(1);
-        when(diurnalTableEntryDay.deleteEntity(any(EntryDayProto.EntryDay.class))).thenReturn(0);
         when(diurnalTableEntryDay.pushNewEntity(any(EntryDayProto.EntryDay.class))).thenReturn(1);
         StopWatch stopWatch = procureStopWatch();
         when(genericConfig.procureStopWatch()).thenReturn(stopWatch);
@@ -87,7 +87,7 @@ public class DataControllerTest {
         ResponsePrimitiveProto.ResponsePrimitive backupPushResult = dataController.pushWholeBackup(
                 DiurnalUtil.generateDataTransit(mobile, email, 20210304, UserMappingProto.Currency.INR,
                         StringUtils.join(testData, "\n")));
-        assertTrue(backupPushResult.getBoolResponse());
+        assertThat(backupPushResult.getBoolResponse()).isTrue();
 
         stopWatch.reset();
         stopWatch.start();
@@ -95,7 +95,7 @@ public class DataControllerTest {
         backupPushResult = dataController.pushWholeBackup(
                 DiurnalUtil.generateDataTransit(mobile, email, 20210304, UserMappingProto.Currency.INR,
                         StringUtils.join(testData, "\n")));
-        assertFalse(backupPushResult.getBoolResponse());
+        assertThat(backupPushResult.getBoolResponse()).isFalse();
     }
 
 }
--- src/test/java/com/vv/personal/diurnal/dbi/util/JsonConverterUtilTest.java
+++ src/test/java/com/vv/personal/diurnal/dbi/util/JsonConverterUtilTest.java
@@ -4,58 +4,58 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import com.vv.personal.diurnal.artifactory.generated.EntryProto;
 import com.vv.personal.diurnal.artifactory.generated.UserMappingProto;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.UUID;
 
 import static com.vv.personal.diurnal.dbi.util.JsonConverterUtil.convertEntryToCompactedJson;
 import static com.vv.personal.diurnal.dbi.util.JsonConverterUtil.convertEntryToJson;
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Vivek
  * @since 06/03/21
  */
-@RunWith(JUnit4.class)
-public class JsonConverterUtilTest {
+@ExtendWith(MockitoExtension.class)
+class JsonConverterUtilTest {
 
     @Test
-    public void testConvertEntryToJson() {
+    void testConvertEntryToJson() {
         EntryProto.Entry entry = DiurnalUtil.generateEntry(123456789L, 20210306, 0, EntryProto.Sign.NEGATIVE, UserMappingProto.Currency.INR, 121.12, "Sampler s1");
         String json = convertEntryToJson(entry);
         System.out.println(json);
-        assertEquals("{\n" +
+        assertThat(json).hasToString("{\n" +
                 "  \"mobile\": \"123456789\",\n" +
                 "  \"date\": 20210306,\n" +
                 "  \"currency\": \"INR\",\n" +
                 "  \"amount\": 121.12,\n" +
                 "  \"description\": \"Sampler s1\"\n" +
-                "}", json);
+                "}");
     }
 
     @Test
-    public void testConvertEntryToCompactedJson() {
+    void testConvertEntryToCompactedJson() {
         EntryProto.Entry entry = DiurnalUtil.generateEntry(123456789L, 20210306, 0, EntryProto.Sign.NEGATIVE, UserMappingProto.Currency.INR, 121.12, "Sampler s1");
         String json = convertEntryToCompactedJson(entry);
         System.out.println(json);
-        assertEquals("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\"}", json);
+        assertThat(json).hasToString("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\"}");
 
         entry = DiurnalUtil.generateEntry(123456789L, 20210306, 0, EntryProto.Sign.NEGATIVE, UserMappingProto.Currency.INR, 121.12, "Sampler s1\n second line this is !");
         json = convertEntryToCompactedJson(entry);
         System.out.println(json);
-        assertEquals("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\\n second line this is !\"}", json);
+        assertThat(json).hasToString("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\\n second line this is !\"}");
 
         entry = DiurnalUtil.generateEntry(123456789L, 20210306, 0, EntryProto.Sign.NEGATIVE, UserMappingProto.Currency.INR, 121.12, "Sampler s1\n" +
                 " second line this is !");
         json = convertEntryToCompactedJson(entry);
         System.out.println(json);
-        assertEquals("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\\n second line this is !\"}", json);
+        assertThat(json).hasToString("{\"mobile\": \"123456789\",\"date\": 20210306,\"currency\": \"INR\",\"amount\": 121.12,\"description\": \"Sampler s1\\n second line this is !\"}");
     }
 
     @Test
-    public void testRaw() {
+    void testRaw() {
         EntryProto.Entry entry = EntryProto.Entry.newBuilder().setSerial(0).setDescription("ijafijfiajf").build();
         EntryProto.Entry entry2 = EntryProto.Entry.newBuilder().setSerial(1).setDescription("fojofjof").build();
         EntryProto.Entry entry3 = EntryProto.Entry.newBuilder().setSerial(2).setDescription("dfkakfo").build();
@@ -75,28 +75,27 @@ public class JsonConverterUtilTest {
     }
 
     @Test
-    public void testConvertSqlEntryToProtoEntry() {
+    void testConvertSqlEntryToProtoEntry() {
         String sqlEntry = "{`@%currency`@%: `@%INR`@%,`@%amount`@%: 100.0,`@%description`@%: `@%lunch`@%}";
         EntryProto.Entry convertedEntry = JsonConverterUtil.convertSqlEntryToProtoEntry(sqlEntry);
         System.out.println(convertedEntry);
     }
 
     @Test
-    public void testConvertSqlEntriesToEntryProtoList() {
+    void testConvertSqlEntriesToEntryProtoList() {
         String sqlEntries = "{`@%currency`@%: `@%INR`@%,`@%amount`@%: 100.0,`@%description`@%: `@%lunch`@%}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 110.0,`@%description`@%: `@%surf excel matic 500gm`@%,`@%serial`@%: 1}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 30.0,`@%description`@%: `@%2 x bhel`@%,`@%serial`@%: 2}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 159.0,`@%description`@%: `@%pillow`@%,`@%serial`@%: 3}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 155.0,`@%description`@%: `@%cornflakes original`@%,`@%serial`@%: 4}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 19.0,`@%description`@%: `@%channi`@%,`@%serial`@%: 5}%~@{`@%currency`@%: `@%INR`@%,`@%amount`@%: 20.0,`@%description`@%: `@%2 x perk (Rs 5) + 1 munch (Rs 10)`@%,`@%serial`@%: 6}";
         EntryProto.EntryList convertedEntryList = JsonConverterUtil.convertSqlEntriesToEntryProtoList(sqlEntries);
         System.out.println(convertedEntryList);
-        assertEquals(7, convertedEntryList.getEntryCount());
-        assertEquals("channi", convertedEntryList.getEntry(5).getDescription());
+        assertThat(convertedEntryList.getEntryCount()).isEqualTo(7);
+        assertThat(convertedEntryList.getEntry(5).getDescription()).hasToString("channi");
     }
 
     @Test
-    public void test() {
+    void test() {
         System.out.println("amazon@apl".matches("[a-z0-9\\-]+@[a-z0-9]+"));
         System.out.println("money-warden@apl".matches("[a-z0-9-]+@[a-z0-9]+"));
         System.out.println("a@apl".matches("[a-z0-9]{3}+@[a-z0-9]+"));
 
         System.out.println(UUID.randomUUID());
     }
-
 }

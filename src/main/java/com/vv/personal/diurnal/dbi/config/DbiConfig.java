145bde84bbf1826d1e589c428920706d35ec227c
Vivek <vivektheintel@gmail.com>
2021-10-28 13:51:15 +0530

    Pruning unused bean

--- src/main/java/com/vv/personal/diurnal/dbi/config/DbiConfig.java
+++ src/main/java/com/vv/personal/diurnal/dbi/config/DbiConfig.java
@@ -7,12 +7,10 @@ import com.vv.personal.diurnal.dbi.interactor.diurnal.dbi.tables.DiurnalTableEnt
 import com.vv.personal.diurnal.dbi.interactor.diurnal.dbi.tables.DiurnalTableUserMapping;
 import com.vv.personal.diurnal.dbi.repository.EntryDayRepository;
 import com.vv.personal.diurnal.dbi.repository.UserMappingRepository;
-import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 /**
@@ -45,12 +43,6 @@ public class DbiConfig {
     @Qualifier("DiurnalTableEntryDay")
     public DiurnalTableEntryDay diurnalTableEntryDays(EntryDayRepository entryDayRepository) {
         return new DiurnalTableEntryDay(entryDayRepository);
-    }
-
-    @Bean(initMethod = "start")
-    @Scope("prototype")
-    public StopWatch stopWatch() {
-        return new StopWatch();
     }
 
     public int getTrialPeriodDays() {
